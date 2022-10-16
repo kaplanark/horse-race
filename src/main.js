677202483375747cb33f94453b238310d009eebe
kaplanark <kaplanarkan@gmail.com>
2022-10-16 20:08:52 +0300

    main.js configured for vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './main.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './main.scss';
+import App from './App.vue';
+import store from './store/index';
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(store);
+app.mount('#app');
