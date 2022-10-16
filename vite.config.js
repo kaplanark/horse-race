7e14318a6f57518a569eb1b270ebae994d856845
kaplanark <kaplanarkan@gmail.com>
2022-10-16 20:00:33 +0300

    vite.config.js aliases defined

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+			'@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+			'@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+			'@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+			'@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+			'@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+			'pages': fileURLToPath(new URL('./src/pages', import.meta.url))
+		}
+	}
 })
