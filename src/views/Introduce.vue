24133ac943eec808e24054b6d9e2b3490933dc14
kaplanark <kaplanarkan@gmail.com>
2022-10-17 21:43:19 +0300

    added scroll webkit css

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -15,7 +15,7 @@
 	width: 100vw;
 	overflow-y: scroll;
 	overflow-x: hidden;
-	background-color: #ffffff;
+	background-color: var(--color-white);
 	background-image: linear-gradient(#f6f6f6 0.5px, transparent 0.5px), linear-gradient(to right, #f6f6f6 0.5px, #ffffff 0.5px);
 	background-size: 10px 10px;
 
