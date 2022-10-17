0729671c3463a69bcee79cc2ac1a9f92fcbe9680
kaplanark <kaplanarkan@gmail.com>
2022-10-17 21:42:41 +0300

    added scroll webkit css

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -18,5 +18,24 @@
 	background-color: #ffffff;
 	background-image: linear-gradient(#f6f6f6 0.5px, transparent 0.5px), linear-gradient(to right, #f6f6f6 0.5px, #ffffff 0.5px);
 	background-size: 10px 10px;
+
+	&::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	&::-webkit-scrollbar-track {
+		border-radius: 5px;
+		margin: 0.2rem 0;
+		background: var(--color-white);
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background: var(--color-secondary);
+		border-radius: 5px;
+	}
+
+	&::-webkit-scrollbar-thumb:hover {
+		background: var(--color-tertiary);
+	}
 }
 </style>
