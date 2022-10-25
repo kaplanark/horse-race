33b543fbe6b53e2a11af2b9b46cb5e3c2a5afa28
kaplanark <kaplanarkan@gmail.com>
2022-10-25 12:15:26 +0300

    updated startRace funtion and screenWidth function

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { onBeforeMount } from 'vue';
 import { useRaceStore } from '@stores/use-race';
+import screenWidth from '@utils/screenWidth';
 
 const raceStore = useRaceStore();
 
 onBeforeMount(() => {
-	var screenWidth = window.innerWidth;
 	raceStore.setLaneLength(screenWidth);
 });
 </script>
