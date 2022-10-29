2f60af5118f9fd8b13cf19a7bae24c771e76f633
kaplanark <kaplanarkan@gmail.com>
2022-10-29 04:10:33 +0300

    comment lines added

--- src/locales/index.js
+++ src/locales/index.js
@@ -6,12 +6,14 @@ const messages = { tr, en };
 const localStoreLocale = localStorage.getItem('locale');
 
 if (!localStoreLocale) {
+	// Get browser language
 	let browserLanguage = Object.keys(messages).find(key => navigator.language.startsWith(key));
 	browserLanguage ? localStorage.setItem('locale', browserLanguage) : localStorage.setItem('locale', 'en');
+	// If the browser language is not in the messages, we set the default locale to English.
 }
 
 const i18n = createI18n({
-	locale: localStorage.getItem('locale'),
+	locale: localStoreLocale || 'en',
 	fallbackLocale: 'tr',
 	messages,
 	globalInjection: true,
